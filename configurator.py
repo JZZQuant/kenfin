830c183c855d7f91d7c0a257563601acf4f05c75
vapnik <v00d00childblues1@gmail.com>
2019-02-04 22:28:55 +0530

    Configurator for returning session and token id requires chromedriver  to be passed as an argument

---
+++ configurator.py
@@ -0,0 +1,82 @@
+from kiteconnect import KiteConnect
+from selenium import webdriver
+import selenium as se
+import time
+import urllib.parse as urlparse
+import os
+import argparse
+
+api_key = "zka582z590jag8yh"
+secret_key = "9zdlmklim6rsakd2fkhay59hybsm5mw6"
+u_id = "RD0291"
+password = "Divakar@1983"
+
+
+class Configurator(object):
+    def __init__(self, driver_path=None):
+        # look for the readme file to get automated headless chromedrive
+        options = se.webdriver.ChromeOptions()
+        options.add_argument('headless')
+        if driver_path is None:
+            self.driver = se.webdriver.Chrome(chrome_options=options)
+        else:
+            self.driver = se.webdriver.Chrome(executable_path=driver_path, chrome_options=options)
+
+    def fill_second_factor_auth_question(self, index):
+        question = self.driver.find_elements_by_tag_name('label')[index].text.lower()
+        answer = self.driver.find_elements_by_tag_name('input')[index]
+        ans = ""
+        if "graduation" in question:
+            ans = "2006"
+        if "mobile" in question:
+            ans = "apple"
+        if "tv" in question:
+            ans = "lg"
+        if "insurance" in question:
+            ans = "sbi"
+        if "bank" in question:
+            ans = "icici"
+        answer.send_keys(ans)
+
+    def get_config(self):
+        kite = KiteConnect(api_key, secret_key)
+        url = kite.login_url()
+
+        # Hit the first url and get session_id
+        self.driver.get(url)
+        session_id = self.parse_url("sess_id")
+
+        # login to first auth page
+        user_id = self.driver.find_elements_by_tag_name("input")[0]
+        pwd = self.driver.find_elements_by_tag_name("input")[1]
+        login = self.driver.find_element_by_xpath('//button[@type="submit"]')
+        user_id.send_keys(u_id)
+        pwd.send_keys(password)
+        login.click()
+
+        # Wait for second auth page to load and fill the second factor authentication question
+        time.sleep(15)
+        self.fill_second_factor_auth_question(0)
+        self.fill_second_factor_auth_question(1)
+        login.click()
+
+        # return request token from final url
+        request_token = self.parse_url("request_token")
+
+        return session_id, request_token
+
+    def parse_url(self, key):
+        time.sleep(20)
+        session_url = self.driver.current_url
+        parsed = urlparse.urlparse(session_url)
+        value = urlparse.parse_qs(parsed.query)[key][0]
+        return value
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--driver_path", help="Custom path to the chrome driver for headless running",
+                        default=None)
+    args = parser.parse_args()
+    session, request = Configurator(args.driver_path).get_config()
+    print("current login session id: '%s' and request token: '%s' " % (session, request))
---
+++ readme.md
@@ -0,0 +1,12 @@
+Install one of the below drivers or download driver to a locaiton  
+brew install geckodriver  
+brew cask install phantomjs  
+brew cask install chromedriver  (prefered)  
+for using selenium on mac/linux  
+or  
+download chromedriver from https://chromedriver.storage.googleapis.com/index.html?path=2.46/
+and pass it to systempath %path  
+or  
+if you cant set systempath just pass it as an argument to the configurator  
+usage confirgurator:  
+python configurator.py -c /Users/vapnik/downloads/chromedriver
