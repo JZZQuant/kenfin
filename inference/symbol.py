c670b353974ee63f15b5ae71af943dfe64ef7cad
vapnik <v00d00childblues1@gmail.com>
2019-02-07 19:56:39 +0530

    comments /todos for @varun

--- inference/symbol.py
+++ inference/symbol.py
@@ -6,10 +6,14 @@ class Symbol(object):
         self.data = self.load_data(self.symbol)
 
     def load_model(self, symbol):
+        #todo : just load the model from a pkl file
         return "loaded model from pkl file"
 
     def symbol_action(self):
+        #todo: this function should hit the kite api and get the latest data and ping the mode take a decision
+        #and append back the data to seld.data dataframe
         print("Symbol loaded %s" % self.symbol)
 
     def load_data(self, symbol):
+        #todo : build the self.datadataframe early in the morning
         return "loaded daily data in the morning"
